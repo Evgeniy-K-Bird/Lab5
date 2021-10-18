37991dc2f7a489dce3e0cdf0a5900534f6bbad84
Evgeniy Idt <evgeniy.k.bird@gmail.com>
2021-10-18 22:46:50 +0300

    Добавьте файлы проекта.

---
+++ Lab5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите массив из 7 чисел");
+            Console.WriteLine();
+            int n = 7;
+            int o = 1;
+            double[] array = new double[n];
+            for (int i = 0; i <= n - 1; i++, o++)
+            {
+                Console.Write("Впишите значение {0}:    ", o);
+
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            double itog = 0;
+            for (int i = 0; i <= n - 1; i++)
+            {
+                itog = itog + array[i];
+            }
+            itog = itog / n;
+            Console.WriteLine();
+            Console.Write("Среднее арифмитическое = {0}", itog);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ z5/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace z5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение N, для матрицы размером NxN:");
+            Console.WriteLine();
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[,] array = new int[n, n];
+            int[] array1 = new int[n];
+            int[] array2 = new int[n];
+            bool o = false;
+            bool o1 = !o;
+            int b = 0;
+            int i = 0;
+            int j = 0;
+            for (i = 0; i < n; i++)
+            {
+                if (n%2==0)
+                {
+                    o = !o;
+                }
+                else
+                {
+                    o1 = !o1;
+                }
+                Console.WriteLine();
+                for (j = 0; j < n; j++, o = !o)
+                {
+                    if (o == true)
+                    {
+                        b = 0;
+                    }
+                    else
+                    {
+                        b = 1;
+                    }
+                    array[i, j] = b;
+                    Console.Write("{0} ", array[i, j]);
+                }
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+            }
+}
+
+        
